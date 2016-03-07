7e8e6d904dd2354dad8c6863430d4d55d272c428
RevoArma3 <robertseibel@outlook.de>
2016-03-07 16:47:39 +0100

    Added files via upload

---
+++ compositions/camps.hpp
@@ -0,0 +1,43 @@
+class Revo_compositions_camps
+{
+	name = "Camps";
+	class campSmall
+	{
+		name = "Camp (Small)";
+		icon = "\a3\Ui_f\data\Map\VehicleIcons\iconVehicle_ca.paa";
+		side = 8;	
+		class Object1{side=8;vehicle="Campfire_burning_F";rank="";position[]={0.168457,-0.0216217,0.489786};dir=0;};
+		class Object2{side=8;vehicle="Land_WoodPile_F";rank="";position[]={5.49023,-0.0821838,0.459748};dir=0;};
+		class Object3{side=8;vehicle="Land_WoodenLog_F";rank="";position[]={5.57935,1.69106,0.459787};dir=0;};
+		class Object4{side=8;vehicle="Land_Sleeping_bag_F";rank="";position[]={1.89551,1.90852,0.459787};dir=298;};
+		class Object5{side=8;vehicle="Land_Sleeping_bag_F";rank="";position[]={-1.17017,2.25948,0.459787};dir=271;};
+		class Object6{side=8;vehicle="Land_Sleeping_bag_F";rank="";position[]={-2.31104,0.532257,0.459787};dir=4;};
+		class Object7{side=8;vehicle="Land_Sleeping_bag_F";rank="";position[]={-0.66748,-2.54115,0.459786};dir=247;};
+		class Object8{side=8;vehicle="Land_Sleeping_bag_F";rank="";position[]={3.07495,-0.651413,0.459794};dir=37;};
+		class Object9{side=8;vehicle="Land_Axe_F";rank="";position[]={5.68921,2.16626,0.459788};dir=0;};
+		class Object10{side=8;vehicle="Land_Bucket_F";rank="";position[]={5.04395,1.4032,0.459787};dir=0;};
+		class Object11{side=8;vehicle="Land_ButaneTorch_F";rank="";position[]={4.73096,1.82985,0.459787};dir=0;};
+		class Object12{side=8;vehicle="Land_CanisterFuel_F";rank="";position[]={5.48315,-1.233,0.459773};dir=0;};
+		class Object13{side=8;vehicle="Land_FoodContainer_01_F";rank="";position[]={5.38257,-1.68588,0.459795};dir=0;};
+		class Object14{side=8;vehicle="Land_GasCooker_F";rank="";position[]={1.66211,0.0696259,0.459787};dir=0;};
+		class Object15{side=8;vehicle="Land_BottlePlastic_V1_F";rank="";position[]={1.55518,1.39915,0.459787};dir=0;};
+		class Object16{side=8;vehicle="Land_BottlePlastic_V1_F";rank="";position[]={-1.62671,0.60939,0.459787};dir=0;};
+		class Object17{side=8;vehicle="Land_Map_F";rank="";position[]={1.8689,-0.330261,0.459781};dir=17;};
+		class Object18{side=8;vehicle="Land_TinContainer_F";rank="";position[]={1.60059,-0.190186,0.459787};dir=0;};
+		class Object19{side=8;vehicle="Land_WaterPurificationTablets_F";rank="";position[]={2.00317,0.0852203,0.459787};dir=0;};
+		class Object20{side=8;vehicle="Land_WaterPurificationTablets_F";rank="";position[]={1.71436,0.248123,0.459787};dir=0;};
+		class Object21{side=8;vehicle="Land_Tablet_02_F";rank="";position[]={2.65063,-0.0652771,0.459787};dir=170;};
+		class Object22{side=8;vehicle="Land_Can_Dented_F";rank="";position[]={0.872559,1.05626,0.459788};dir=0;};
+		class Object23{side=8;vehicle="Land_Can_Dented_F";rank="";position[]={-0.172119,-1.00082,0.459787};dir=0;};
+		class Object24{side=8;vehicle="Land_Can_Rusty_F";rank="";position[]={2.18408,-0.513199,0.459763};dir=0;};
+		class Object25{side=8;vehicle="Land_Matches_F";rank="";position[]={1.97119,-0.13826,0.459787};dir=0;};
+		class Object26{side=8;vehicle="Land_Matches_F";rank="";position[]={2.09888,-0.0951385,0.459788};dir=0;};
+		class Object27{side=8;vehicle="Land_BakedBeans_F";rank="";position[]={1.65942,-0.40831,0.459754};dir=0;};
+		class Object28{side=8;vehicle="Land_BagFence_Round_F";rank="";position[]={-4.59277,4.51509,0.458486};dir=128;};
+		class Object29{side=8;vehicle="Land_Matches_F";rank="";position[]={-0.130371,-2.01588,0.467284};dir=0;};
+		class Object30{side=8;vehicle="Land_Matches_F";rank="";position[]={0.026123,2.40829,0.465878};dir=173.999;};
+		class Object31{side=8;vehicle="Land_BagFence_Round_F";rank="";position[]={5.14673,-3.62044,0.458486};dir=302;};
+		class Object32{side=8;vehicle="Land_BagFence_Round_F";rank="";position[]={-3.91528,-5.53925,0.458487};dir=32;};
+		class Object33{side=8;vehicle="Land_BagFence_Round_F";rank="";position[]={4.78613,6.21851,0.458487};dir=219;};
+	};
+};
