a44e316325fd32ae636e51021a533b717dc067e6
RevoArma3 <robertseibel@outlook.de>
2016-04-03 22:13:40 +0200

    Optimized saveGear expession
    - changed variable name
    - replaced BIS_fnc_spawn with call

--- 3denEnhanced/objectAttributes/saveGear.hpp
+++ 3denEnhanced/objectAttributes/saveGear.hpp
@@ -3,17 +3,17 @@ class SaveGear: RemoveWeapons
 	displayName = $STR_saveGear_displayName;
 	tooltip = $STR_saveGear_tooltip;
 	property = "Enh_saveGear";
-	expression = 
+	expression =
 	"\
 		_this spawn\
 		{\
 			waitUntil {isPlayer _this};\
 			{\
-				[player,[missionNamespace,'inventory_var']] call BIS_fnc_saveInventory;\
+				[player,[missionNamespace,'Enh_savedInventory']] call BIS_fnc_saveInventory;\
 				player addEventHandler ['respawn', {\
-					[_this select 0,[missionNamespace,'inventory_var']] call BIS_fnc_loadInventory;\
+					[_this select 0,[missionNamespace,'Enh_savedInventory']] call BIS_fnc_loadInventory;\
 				}];\
-			} remoteExec ['BIS_fnc_spawn', owner _this,true];\
+			} remoteExec ['call',owner _this,true];\
 		};\
 	";
 };
