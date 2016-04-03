eaab180d1ff5d70ba9195c92b4d72d8fd972c6f3
RevoArma3 <robertseibel@outlook.de>
2016-04-03 22:18:42 +0200

    Optimised addGunLight attribute
    - replace select with param to prevent possible failing

--- 3denEnhanced/objectAttributes/addGunLight.hpp
+++ 3denEnhanced/objectAttributes/addGunLight.hpp
@@ -3,11 +3,11 @@ class AddGunLight: RemoveWeapons
 	displayName = $STR_addGunLight_displayName;
 	tooltip = $STR_addGunLight_tooltip;
 	property = "Enh_addGunLight";
-	expression = 
+	expression =
 	"\
 		if(_value) then\
 		{\
-			_this removePrimaryWeaponItem (primaryWeaponItems _this select 1);\
+			_this removePrimaryWeaponItem (primaryWeaponItems param [1,"",[""]]);\
 			_this addPrimaryWeaponItem 'acc_flashlight';\
 			_this enableGunLights 'forceOn';\
 		};\
