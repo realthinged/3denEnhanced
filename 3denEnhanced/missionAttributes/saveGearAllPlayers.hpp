8e37c0f0a23ee92696cd5d73e84adb9a6f91994e
RevoArma3 <robertseibel@outlook.de>
2016-04-07 13:57:21 +0200

    Added saveGearAllPlayers.hpp
    Auto merge failed so I am doing it manually

---
+++ 3denEnhanced/missionAttributes/saveGearAllPlayers.hpp
@@ -0,0 +1,26 @@
+class Respawn
+{
+   class Attributes
+   {
+      class Activate
+      {
+         displayName = $STR_saveGearAllPlayers_displayName;
+         tooltip = $STR_saveGearAllPlayers_tooltip;
+         property = "Enh_saveGearPlayers";
+         control = "Checkbox";
+         expression =
+         "\
+            if(!is3DEN && _value && isMultiplayer) then\
+            {\
+               {\
+                  [player,[missionNamespace,'Enh_savedInventory']] call BIS_fnc_saveInventory;\
+                  player addEventHandler ['respawn', {\
+                     [_this select 0,[missionNamespace,'Enh_savedInventory']] call BIS_fnc_loadInventory;\
+                  }];\
+               } remoteExec ['call',-2,true];\
+            };\
+         ";
+         defaultValue = "false";
+      };
+   };
+};
