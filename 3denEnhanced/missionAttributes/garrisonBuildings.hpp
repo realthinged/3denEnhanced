4d236c90e0044d353b99219f419b0b8ac2bcac29
RevoArma3 <robertseibel@outlook.de>
2016-04-13 00:56:48 +0200

    Update garrisonBuildings.hpp
    - removed attribute for building coverage

--- 3denEnhanced/missionAttributes/garrisonBuildings.hpp
+++ 3denEnhanced/missionAttributes/garrisonBuildings.hpp
@@ -2,30 +2,23 @@ class GarrisonBuildings
 {
 	collapsed = 0;
 	displayName = $STR_garrisonSettings_headline;
-	class attributes
+	class Attributes
 	{
-		class BuildingCoverage
+		class BuildingPosCoverage
 		{
-			displayName = $STR_buildingCoverage_displayName; 
-			tooltip = $STR_buildingCoverage_tooltip; 
-			property = "Enh_coverageBuildings"; 
+			displayName = $STR_buildingPosCoverage_displayName;
+			tooltip = $STR_buildingPosCoverage_tooltip;
+			property = "Enh_coverageBuildingsPos";
 			control = "GarrisonCoverageCombo";
 			expression = "";
 			defaultValue = 1;
 			typeName = "NUMBER";
 		};
-		class BuildingPosCoverage: BuildingCoverage
-		{
-			displayName = $STR_buildingPosCoverage_displayName; 
-			tooltip = $STR_buildingPosCoverage_tooltip; 
-			property = "Enh_coverageBuildingsPos";
-			typeName = "NUMBER";
-		};
 		class GarrisonFaction
 		{
-			displayName = $STR_faction_displayName; 
-			tooltip = $STR_faction_tooltip; 
-			property = "Enh_garrisonFaction"; 
+			displayName = $STR_faction_displayName;
+			tooltip = $STR_faction_tooltip;
+			property = "Enh_garrisonFaction";
 			control = "GarrisonFactionCombo";
 			expression = "";
 			defaultValue = "NATO";
@@ -33,6 +26,3 @@ class GarrisonBuildings
 		};
 	};
 };
-
-
-
