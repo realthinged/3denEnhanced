8658f1b2355beb23266a6c18391185877f7b284f
RevoArma3 <robertseibel@outlook.de>
2016-05-12 22:08:58 +0200

    Update sliderTimeMultiplier.hpp

--- 3denEnhanced/controls/sliderTimeMultiplier.hpp
+++ 3denEnhanced/controls/sliderTimeMultiplier.hpp
@@ -1,3 +1,5 @@
+#define EDIT_W	10
+
 class SliderTimeMultiplier: Title
 {
 	attributeLoad =
@@ -9,7 +11,19 @@ class SliderTimeMultiplier: Title
 		_ctrlEdit ctrlsettext (str ((round (_value * 10)) / 10) + 'x');\
 		_ctrlSlider slidersetposition _value;\
 	";
-	attributeSave = "sliderposition (_this controlsGroupCtrl 100)";
+	attributeSave =
+	"\
+		_value = sliderposition (_this controlsGroupCtrl 100);\
+		if (_value >= 1) then\
+		{\
+			_value = round _value;\
+		}\
+		else\
+		{\
+			_value = [_value,1] call BIS_fnc_cutDecimals;\
+		};\
+		_value;\
+	";
 
 	class Controls: Controls
 	{
@@ -18,10 +32,9 @@ class SliderTimeMultiplier: Title
 		{
 			idc = 101;
 			canModify = 1;
-			x = "(48 + 	82 - 	10) * (pixelW * 1.25 * 4)";
-			y = 0;
-			h = "5 * (pixelH * 1.25 * 4)";
-			w = "10 * (pixelW * 1.25 * 4)";
+			x = (ATTRIBUTE_TITLE_W + ATTRIBUTE_CONTENT_W - EDIT_W) * GRID_W;
+			w = EDIT_W * GRID_W;
+			h = SIZE_M * GRID_H;
 			onKillFocus =
 			"\
 				_ctrlEdit = _this select 0;\
@@ -36,19 +49,27 @@ class SliderTimeMultiplier: Title
 		class Value: ctrlXSliderH
 		{
 			idc = 100;
-			x = "48 * (pixelW * 1.25 * 4)";
-			y = 0;
-			w = "(82 - 10) * (pixelW * 1.25 * 4)";
-			h = "5 * (pixelH * 1.25 * 4)";
+			x = ATTRIBUTE_TITLE_W * GRID_W;
+			w = (ATTRIBUTE_CONTENT_W - EDIT_W) * GRID_W;
+			h = SIZE_M * GRID_H;
          sliderPosition = 1;
-         sliderRange[] = {0.1,120};
+         sliderRange[] = {0,120};
 			onSliderPosChanged =
 			"\
 				_ctrlSlider = _this select 0;\
 				_ctrlGroup = ctrlParentControlsGroup _ctrlSlider;\
 				_ctrlEdit = _ctrlGroup controlsgroupctrl 101;\
 				_value = sliderposition _ctrlSlider;\
-				_ctrlEdit ctrlsettext (str ((round (_value * 10)) / 10) + 'x');\
+				if (_value >= 1) then\
+				{\
+					_value = round _value;\
+					_ctrlEdit ctrlsettext (str _value + 'x');\
+				}\
+				else\
+				{\
+					_value = [_value,1] call BIS_fnc_cutDecimals;\
+					_ctrlEdit ctrlsettext (str _value + 'x');\
+				};\
 			";
 		};
 	};
